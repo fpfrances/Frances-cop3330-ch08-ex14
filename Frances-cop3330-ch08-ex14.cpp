b793f3aa7154432227217e25e656bac9a5b410b1
fpfrances <fpfrances@hotmail.com>
2021-11-22 18:56:02 -0500

    Add project files.

---
+++ Frances-cop3330-ch08-ex14.cpp
@@ -0,0 +1,12 @@
+#include "Header.h"
+void f(const int);
+
+int main()
+{
+	int a1 = 100;
+	const int a2 = 101;
+	f(a1);
+	f(a2);
+	f(102);
+	system("pause>0");
+}
---
+++ Header.h
@@ -0,0 +1,10 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 5 Solution
+ *  Copyright 2021 Filipe Frances
+ */
+#include <iostream>
+
+void f(const int i)
+{
+	std::cout << i << std::endl;
+}
